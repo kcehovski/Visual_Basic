39534f640cd9bcc4cd2584e8a1498c87593fc631
kcehovski <katarina.cehovski@gmail.com>
2015-08-26 15:56:56 +0100

    new project

---
+++ projectWorked/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDemoListBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnQUIT = New System.Windows.Forms.Button()
+        Me.lblBothPrices = New System.Windows.Forms.Label()
+        Me.lstPricePds = New System.Windows.Forms.ListBox()
+        Me.lstPriceEuros = New System.Windows.Forms.ListBox()
+        Me.txtPriceBoth = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnQUIT
+        '
+        Me.btnQUIT.Location = New System.Drawing.Point(271, 517)
+        Me.btnQUIT.Name = "btnQUIT"
+        Me.btnQUIT.Size = New System.Drawing.Size(75, 23)
+        Me.btnQUIT.TabIndex = 0
+        Me.btnQUIT.Text = "Quit"
+        Me.btnQUIT.UseVisualStyleBackColor = True
+        '
+        'lblBothPrices
+        '
+        Me.lblBothPrices.AutoSize = True
+        Me.lblBothPrices.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBothPrices.Location = New System.Drawing.Point(240, 253)
+        Me.lblBothPrices.Name = "lblBothPrices"
+        Me.lblBothPrices.Size = New System.Drawing.Size(116, 24)
+        Me.lblBothPrices.TabIndex = 1
+        Me.lblBothPrices.Text = "Both Prices"
+        '
+        'lstPricePds
+        '
+        Me.lstPricePds.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstPricePds.FormattingEnabled = True
+        Me.lstPricePds.ItemHeight = 14
+        Me.lstPricePds.Location = New System.Drawing.Point(55, 12)
+        Me.lstPricePds.Name = "lstPricePds"
+        Me.lstPricePds.Size = New System.Drawing.Size(217, 186)
+        Me.lstPricePds.TabIndex = 2
+        '
+        'lstPriceEuros
+        '
+        Me.lstPriceEuros.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstPriceEuros.FormattingEnabled = True
+        Me.lstPriceEuros.ItemHeight = 14
+        Me.lstPriceEuros.Location = New System.Drawing.Point(312, 12)
+        Me.lstPriceEuros.Name = "lstPriceEuros"
+        Me.lstPriceEuros.Size = New System.Drawing.Size(214, 186)
+        Me.lstPriceEuros.Sorted = True
+        Me.lstPriceEuros.TabIndex = 3
+        '
+        'txtPriceBoth
+        '
+        Me.txtPriceBoth.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtPriceBoth.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPriceBoth.Location = New System.Drawing.Point(144, 303)
+        Me.txtPriceBoth.Multiline = True
+        Me.txtPriceBoth.Name = "txtPriceBoth"
+        Me.txtPriceBoth.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtPriceBoth.Size = New System.Drawing.Size(308, 192)
+        Me.txtPriceBoth.TabIndex = 4
+        '
+        'frmDemoListBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 561)
+        Me.Controls.Add(Me.txtPriceBoth)
+        Me.Controls.Add(Me.lstPriceEuros)
+        Me.Controls.Add(Me.lstPricePds)
+        Me.Controls.Add(Me.lblBothPrices)
+        Me.Controls.Add(Me.btnQUIT)
+        Me.Name = "frmDemoListBox"
+        Me.Text = "DEMO ListBoxes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnQUIT As System.Windows.Forms.Button
+    Friend WithEvents lblBothPrices As System.Windows.Forms.Label
+    Friend WithEvents lstPricePds As System.Windows.Forms.ListBox
+    Friend WithEvents lstPriceEuros As System.Windows.Forms.ListBox
+    Friend WithEvents txtPriceBoth As System.Windows.Forms.TextBox
+
+End Class
---
+++ projectWorked/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class frmDemoListBox
+
+    Private Sub frmDemoListBox_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstPricePds.Items.Add("Potato 149")
+        lstPricePds.Items.Add("Red Pepper 343")
+        lstPricePds.Items.Add("Cauliflower 193")
+        lstPricePds.Items.Add("Carrot 075")
+        lstPricePds.Items.Add("Pea 132")
+        lstPricePds.Items.Add("Courgette 207")
+        lstPricePds.Items.Add("Onion 168")
+        lstPricePds.Items.Add("Green Bean 243")
+        lstPricePds.Items.Add("Mushroom 243")
+        lstPricePds.Items.Add("Broccoli 203")
+    End Sub
+
+    Private Sub btnQUIT_Click(sender As Object, e As EventArgs) Handles btnQUIT.Click
+        Application.Exit()
+    End Sub
+
+
+    Private Sub lstPricePds_DoubleClick(sender As Object, e As EventArgs) Handles lstPricePds.DoubleClick
+        Dim Temp, Veg As String
+        Dim Pence, Pounds, Cents As String
+        Dim Euros, Converted, TwoPrices As String
+        Dim ExchangeRate As String
+
+        ExchangeRate = 1.14
+
+        Temp = lstPricePds.Text
+        Veg = Temp.Substring(0, Temp.Length - 3)
+        Pence = Temp.Substring(Temp.Length - 3)
+        Cents = Format(ExchangeRate * CInt(Pence), "000")
+
+        Converted = Veg & Cents
+        lstPriceEuros.Items.Add(Converted)
+
+        Pounds = Format(CInt(Pence) / 100, "0.00")
+        Euros = Format(CInt(Cents) / 100, "0.00")
+        TwoPrices = Veg & " £" & Pounds & " = " & "€ " & Euros
+        txtPriceBoth.Text = txtPriceBoth.Text & TwoPrices & vbCrLf
+    End Sub
+End Class
