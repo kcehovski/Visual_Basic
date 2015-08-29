b6a7aeac6fbc2b05815ef813d4a26cebf089e516
kcehovski <katarina.cehovski@gmail.com>
2015-08-29 14:17:52 +0200

    new project

---
+++ projectReadingAndWriting/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(12, 12)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtOutput.Size = New System.Drawing.Size(269, 248)
+        Me.txtOutput.TabIndex = 0
+        '
+        'btnRead
+        '
+        Me.btnRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRead.Location = New System.Drawing.Point(308, 61)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(75, 54)
+        Me.btnRead.TabIndex = 1
+        Me.btnRead.Text = "Read from Database"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnWrite
+        '
+        Me.btnWrite.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnWrite.Location = New System.Drawing.Point(308, 121)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(75, 54)
+        Me.btnWrite.TabIndex = 2
+        Me.btnWrite.Text = "Write to file"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Location = New System.Drawing.Point(308, 237)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 356)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents btnRead As System.Windows.Forms.Button
+    Friend WithEvents btnWrite As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ projectReadingAndWriting/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+    Dim connectionString As String
+    Dim cnn As New SqlConnection
+    Dim dt As New DataTable
+    Dim iIndex As Integer = 0
+    Dim iRecCount As Integer
+
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+
+        Dim sw As StreamWriter
+        Dim sWriteFIle As String
+
+        sWriteFIle = "..\..\FromDatabase.txt"
+        If File.Exists(sWriteFIle) Then
+            File.Delete(sWriteFIle)
+        End If
+        sw = New StreamWriter(sWriteFIle, True)
+
+        connectionString = "Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Kaja\Documents\Visual_Basic\projectReadingAndWriting\Database.mdf;Integrated Security=True"
+        cnn = New SqlConnection(connectionString)
+
+        Try
+            cnn.Open()
+            Dim str As String = "SELECT * FROM Customers"
+            Dim da As New SqlDataAdapter(str, cnn)
+            da.Fill(dt)
+            iRecCount = dt.Rows.Count
+
+            For Me.iIndex = 0 To iRecCount - 1
+                sw.Write(Trim(dt.Rows(iIndex).Item(2)) & ", " &
+                         Trim(dt.Rows(iIndex).Item(1)) & vbNewLine)
+            Next iIndex
+
+            sw.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            If cnn.State = ConnectionState.Open Then
+                cnn.Close()
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        If cnn.State() = ConnectionState.Open Then
+            cnn.Close()
+        End If
+        Me.Close()
+    End Sub
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        WriteToTextBox()
+        WriteToTextFile()
+    End Sub
+
+    Private Sub WriteToTextBox()
+        Dim sr As StreamReader
+        Dim sReadFile As String
+
+        sReadFile = "..\..\FromDatabase.txt"
+
+        Try
+            sr = New StreamReader(sReadFile)
+            Dim strLine As String = ""
+
+            Do While sr.Peek() <> -1
+                strLine = strLine & sr.ReadLine & vbNewLine
+            Loop
+            txtOutput.Text = strLine
+            sr.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub WriteToTextFile()
+        Dim sr As StreamReader
+        Dim sReadFile As String
+        Dim sw As StreamWriter
+        Dim sWriteFile As String
+
+        sReadFile = "..\..\FromDatabase.txt"
+        sWriteFile = "..\..\Output.txt"
+
+        If File.Exists(sWriteFile) Then
+            File.Delete(sWriteFile)
+        End If
+
+        Try
+            sr = New StreamReader(sReadFile)
+            sw = New StreamWriter(sWriteFile)
+
+            Dim strLine As String = ""
+            Do While sr.Peek() <> -1
+                strLine = sr.ReadLine() & vbNewLine
+                sw.Write(strLine)
+            Loop
+
+            sr.Close()
+            sw.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+End Class
